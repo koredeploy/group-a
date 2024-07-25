dc4190c5865aabd8bc198350ffcbf144d34bb948
onlyeugene <eugeneokogun@gmail.com>
2024-07-25 12:03:08 +0100

    feat: designed and routed the internalmpage layout

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,41 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import axiosInstance from '../../utils/axiosInstance'
-import MenuCard from './MenuCard';
+// import React, { useEffect, useState } from 'react';
+// import axiosInstance from '../../utils/axiosInstance'
+// import MenuCard from './MenuCard';
 
-const Menu = () => {
-  const [menuItems, setMenuItems] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+// const Menu = () => {
+//   const [menuItems, setMenuItems] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchMenuItems = async () => {
-      try {
-        const response = await axiosInstance.get('/api/product'); // Adjust the endpoint as needed
-        setMenuItems(response.data);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
+//   useEffect(() => {
+//     const fetchMenuItems = async () => {
+//       try {
+//         const response = await axiosInstance.get('/api/product'); // Adjust the endpoint as needed
+//         setMenuItems(response.data);
+//       } catch (error) {
+//         setError(error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
 
-    fetchMenuItems();
-  }, []);
+//     fetchMenuItems();
+//   }, []);
 
-  if (loading) return <p className='text-center'>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+//   if (loading) return <p className='text-center'>Loading...</p>;
+//   if (error) return <p>Error: {error.message}</p>;
 
-  return (
-    <div className='py-[2rem]'>
-      <h1 className='text-5xl font-medium text-center'>Special Menu</h1>
-      <p className='text-lg mx-auto w-[37rem] text-center'>Discover our specially curated menu, designed to delight your taste buds with unique and seasonal dishes</p>
-      <div className='grid grid-cols-4 justify-  gap-[2rem] mt-10 px-[7rem]'>
-        {menuItems.map((item ,key) => 
-            <MenuCard key={key} data={item}/>
-        )}
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className='py-[2rem]'>
+//       <h1 className='text-5xl font-medium text-center'>Special Menu</h1>
+//       <p className='text-lg mx-auto w-[37rem] text-center'>Discover our specially curated menu, designed to delight your taste buds with unique and seasonal dishes</p>
+//       <div className='grid grid-cols-4 justify-  gap-[2rem] mt-10 px-[7rem]'>
+//         {menuItems.map((item ,key) => 
+//             <MenuCard key={key} data={item}/>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Menu;
+// export default Menu;
